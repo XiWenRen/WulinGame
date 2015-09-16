f43c9718ac0ad1b6c6764f886b684bf878c152ea
XiWenRen <562661751@qq.com>
2015-09-16 22:16:00 +0800

    添加场景功能

---
+++ src/main/java/com/wulin/application/Scene.java
@@ -0,0 +1,14 @@
+package com.wulin.application;
+
+/**
+ * 场景的接口
+ */
+public interface Scene {
+
+    void init();
+
+    void destory();
+
+    void run();
+
+}
---
+++ src/main/java/com/wulin/application/impl/ExerciseScene.java
@@ -0,0 +1,28 @@
+package com.wulin.application.impl;
+
+import com.wulin.application.Scene;
+
+/**
+ * 练功场景类
+ * 可以有多种练功模式，比如说番茄钟模式、答题模式等，对应不同的属性增长
+ * 练功的基本动作应该是
+ * 1.获取练功模式
+ * 2.储存练功进度信息，包括人物属性增长/降低
+ * 3.中途如果失败的处理
+ */
+public class ExerciseScene implements Scene{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destory() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
